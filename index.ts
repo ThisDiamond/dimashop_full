b2213eed475a2ad73f08b4bd4314cb4832d65fcc
Dilshodbek <Dilshodbeknodejs@gmail.com>
2023-03-21 00:49:15 +0500

    add uuid, send alerts register and login

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express";
 import { engine, create } from 'express-handlebars'
+import session from "express-session";
 import dotenv from 'dotenv'
 import cors from 'cors'
+
+import flash from 'connect-flash'
 
 import AuthRoutes from './routes/auth'
 import ProductsRoutes from './routes/products'
@@ -9,7 +12,6 @@ import ProductsRoutes from './routes/products'
 dotenv.config()
 
 const server: Express = express()
-
 const hbs = create({ defaultLayout: 'main', extname: 'hbs' })
 
 
@@ -20,8 +22,9 @@ server.set('views', './views')
 server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
-
 server.use(express.static('public'))
+server.use(session({ secret: "secret", resave: false, saveUninitialized: false }))
+server.use(flash());
 
 server.use(AuthRoutes)
 server.use(ProductsRoutes)
@@ -34,3 +37,4 @@ const StartServer = () => {
 }
 
 StartServer()
+
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import Joi from 'joi'
 import db from '../model/database'
+import { v4 } from 'uuid'
 import bcrypt from 'bcrypt'
 
 const router = Router()
@@ -8,30 +9,47 @@ const router = Router()
 router.get('/register', (req, res) => {
     res.render(
         'register',
-        { title: 'Register', isRegister: true, registerError: 'Error' }
+        { title: 'Register', isRegister: true, registerError: req.flash('registerError') }
     )
 })
 router.get('/login', (req, res) => {
     res.render(
         'login',
-        { title: 'Login', isLogin: true, loginError: 'Error' }
+        { title: 'Login', isLogin: true, loginError: req.flash('loginError') }
     )
 })
 
 
 router.post('/login', async (req, res) => {
     const body = req.body;
+    const { email, password } = body;
+
+    if (!email && !password) {
+        req.flash('loginError', 'Email va parol kiriting')
+        res.redirect('/login')
+        return
+    }
 
     interface UserJoi { email: string; password: string }
 
     const loginSchema = Joi.object<UserJoi>({
         email: Joi.string().required().min(6).max(32).regex(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/),
-        password: Joi.string().required().min(6).max(32).regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,32}$/)
+        password: Joi.string().required().min(6).max(32).regex(/^[a-zA-Z0-9]{6,32}$/)
     });
 
     const { error, value } = loginSchema.validate(body);
 
-    if (error) { console.error(error.details[0].message); res.redirect('/login') }
+    if (error) {
+        let flash: string = ''
+        if (`"email" is not allowed to be empty` == error.details[0].message) {
+            flash = 'Emailingizni yozing'
+        } else if (`"password" is not allowed to be empty` == error.details[0].message) {
+            flash = 'Parol kiritilishi shart'
+        }
+        req.flash('loginError', flash)
+        res.redirect('/login')
+        return
+    }
     else { login() }
 
     function login() {
@@ -42,13 +60,22 @@ router.post('/login', async (req, res) => {
         db.get('SELECT email, password FROM users WHERE email=?', [body.email], async (err, row: User) => {
             if (err) return console.log(err);
             if (row) {
-                console.log(`user found `);
-                const isPasswordEqual = await bcrypt.compare(body.password, String(row.password))
-                if (!isPasswordEqual) { console.log('password wrong') }
-                else { console.log(`user ${row.email} pass ${row.password}`); }
-            } else { console.log('user not found'); }
+                // user found and check
+                const isPasswordEqual = await bcrypt.compare(body.password, String(row.password));
+                if (!isPasswordEqual) {
+                    req.flash('loginError', 'Parol xato:')
+                    res.redirect('/login')
+                } else { // user found all check complated
+                    req.flash('loginError', `user ${row.email} pass ${row.password}`)
+                    res.redirect('/login')
+                }
+            }
+            if (!row) {
+                req.flash('loginError', 'Foydalanuvchi mavjud emas')
+                res.redirect('/login')
+            }
         })
-        res.redirect('/')
+
     }
 
 })
@@ -57,27 +84,40 @@ router.post('/register', async (req, res) => {
     const body = req.body
     const hashedPassword = await bcrypt.hash(body.password, 10)
 
+    const { firstname, lastname, email, password } = body
+    if (!firstname || !lastname || !email || !password) {
+        req.flash('registerError', `Hamma maydonlar to'ldirilishi shart:`)
+        res.redirect('/register')
+        return
+    }
+
     interface RegJoi { firstname: string, lastname: string, email: string, password: string, }
     const RegisterSchema = Joi.object<RegJoi>({
         firstname: Joi.string().required().min(3).max(16).pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/),
         lastname: Joi.string().min(3).max(20).pattern(/^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/),
         email: Joi.string().required().min(6).max(32).regex(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/),
-        password: Joi.string().required().min(6).max(32).regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,32}$/)
+        password: Joi.string().required().min(6).max(32).regex(/^[a-zA-Z0-9]{6,32}$/)
     })
     const { error, value } = RegisterSchema.validate(body)
 
-    if (error) { console.error(error.details[0].message); res.redirect('/register') }
+    if (error) {
+        req.flash('registerError', error.details[0].message)
+        res.redirect('/register')
+        return
+    }
     else { register() }
 
     function register() {
         interface User { email: string; }
-        db.get('SELECT email, password FROM users WHERE email=?', [body.email], async (err, row: User) => {
+        db.get('SELECT email, password FROM users WHERE email=?', [email], async (err, row: User) => {
             if (err) return console.log(err);
             if (row) {
-                console.log('user  found');
+                req.flash('registerError',`Bunday foydalanuvchi ro'yxatdan o'tgan:`)
+                res.redirect('/register')
+                return
             } else {
-                const sql = 'INSERT INTO users(firstname, lastname, email, password) VALUES(?,?,?,?)';
-                const params = [body.firstname, body.lastname, body.email, hashedPassword]
+                const sql = 'INSERT INTO users(id, firstname, lastname, email, password) VALUES(?,?,?,?,?)';
+                const params = [v4(), firstname, lastname, email, hashedPassword]
                 db.run(sql, params, (err) => {
                     if (err != null) console.log(err);
                 })
